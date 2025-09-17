2c079cc682a3ee6af4d78c43c431086425e2ecfb
sdiasova <diasova@sunlight.cz>
2025-09-18 00:35:18 +0200

    hero animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import {useGSAP} from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import {useMediaQuery} from "react-responsive";
 
 const Hero = () => {
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({maxWidth: 767});
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title', {
             type: 'chars, words'
@@ -38,7 +42,28 @@ const Hero = () => {
             }
         }).to('.right-leaf', { y: 200 }, 0)
             .to('.left-leaf', { y: -200 }, 0)
-    }, [])
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% top' : 'bottom top';
+
+        // Video animation timeline
+        // Create the timeline with a default duration
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration
+            })
+        }
+    }, []);
     return (
        <>
            <section id="hero" className="noisy">
@@ -72,6 +97,16 @@ const Hero = () => {
                    </div>
                </div>
            </section>
+
+           <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/videos/output.mp4"
+                    muted
+                    playsInline
+                    preload="auto"
+                />
+           </div>
        </>
     )
 }
