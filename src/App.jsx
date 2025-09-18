f1888a6135f3f2db94480e0873ae6f5800fea105
sdiasova <diasova@sunlight.cz>
2025-09-19 00:10:13 +0200

    about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import gsap from "gsap";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,6 +14,7 @@ const App = () => {
             <Navbar />
             <Hero />
             <Cocktails />
+            <About />
         </main>
     )
 }
